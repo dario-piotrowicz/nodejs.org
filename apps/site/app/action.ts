d065f8a495e4a469bab8e2a63ebcd6d19fd11af0
Dario Piotrowicz <dario@cloudflare.com>
2025-07-21 23:37:55 +0100

    005

--- apps/site/app/action.ts
+++ apps/site/app/action.ts
@@ -3,5 +3,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function testAction(message: string) {
   'use server';
-  return `from server action 004 (${new Date().toISOString()})`;
+  return `from server action 005 (${new Date().toISOString()})`;
 }
